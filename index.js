689e29f83794d923eb3fdde7614a53e1e04e9a6b
Ephrem Demelash <demelashephrem@gmail.com>
2023-05-04 08:10:15 +0000

    set up telegram webhook and route to accept updates

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-require('dotenv').config();
+require('dotenv').config({
+    path: `.env`,
+});
 const server = require('./src/server/server');
+const connectDb = require('./src/config/db');
+const { initUserIdsCache } = require('./src/utils/cacheUser');
 const bot = require('./src/bot/bot');
 
-server.startServer();
-bot.launch();
+
+(async () => {
+    await bot.telegram.setWebhook(`${process.env.WEBHOOK_URL}/telegram-webhook`)
+
+    await connectDb();
+    await initUserIdsCache();
+    server.startServer()
+})()
