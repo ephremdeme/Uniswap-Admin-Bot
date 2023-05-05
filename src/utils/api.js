9c6b97fe00f273daae9a9727f29d3eff0fa8160b
Ephrem Demelash <demelashephrem@gmail.com>
2023-05-05 20:57:37 +0000

    remove timeout conf

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 
 const axiosInstance = axios.create({
     baseURL: process.env.API_URL,
-    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
         "Authorization": `Bearer ${process.env.API_TOKEN}`
